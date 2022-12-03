46785f2871e842b42c8b29ec0d27bb75415d2e51
nblackbu <sitdikovaelvinaa96@gmail.com>
2022-12-03 22:14:50 +0300

    lesson2

--- Lesson2/WordStat.java
+++ Lesson2/WordStat.java
@@ -5,7 +5,6 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class WordStat {
